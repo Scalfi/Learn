0f38f582e1ef247da5a7a058636a5ebf2291573e
Scalfi <gascalfi@gmail.com>
2020-11-01 20:08:08 -0300

    repositorio produto finish

--- Repositories/ProdutoRepository.cs
+++ Repositories/ProdutoRepository.cs
@@ -1,11 +1,100 @@
-﻿using System;
+﻿using Learn.Models;
+using Learn.Models.Database;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Learn.Repositories
 {
+
     public class ProdutoRepository
     {
+        private readonly MyDbContext _db;
+        private readonly CategoriaRepository _categoriaRepository;
+
+        public ProdutoRepository(MyDbContext db)
+        {
+            _db = db;
+        }
+        public async Task<Produto> PegaProdutoAsync(int id)
+        {
+            return await _db.Produtos
+                        .Where(p => p.ProdutoId.Equals(id))
+                        .FirstOrDefaultAsync();
+        }
+
+        public async Task<List<Produto>> PegaProdutosAsync(int[] ids)
+        {
+            return await _db.Produtos
+                        .Where(p => ids.Contains(p.ProdutoId))
+                        .ToListAsync();
+        }
+
+
+        public async Task<Produto> CriaProdutoAsync(Produto produto) 
+        {
+            var novoProduto = new Produto
+            {
+                Nome = produto.Nome,
+                Descricao = produto.Descricao,
+                Valor = produto.Valor,
+                Marca = produto.Marca
+            };
+
+            try
+            {
+                await _db.AddAsync(novoProduto);
+                await _db.SaveChangesAsync();
+                return novoProduto;
+            } catch( Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+
+        }
+
+
+        public async Task<Produto> AtualizaProdutoAsync(Produto atualizaProduto, Produto produto)
+        {
+            produto.Nome = atualizaProduto.Nome;
+            produto.Descricao = atualizaProduto.Descricao;
+            produto.Valor = atualizaProduto.Valor;
+            produto.Marca = atualizaProduto.Marca;
+
+            try
+            {
+                await _db.AddAsync(produto);
+                await _db.SaveChangesAsync();
+                return produto;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+
+        }
+
+
+        public async Task<Produto> DeletaProdutoAsync(Produto produto)
+        {
+ 
+            try
+            {
+                 _db.Remove(produto);
+                await _db.SaveChangesAsync();
+                return produto;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+
+        }
+
     }
 }
