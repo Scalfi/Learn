be4911bc0f2a9e1f108a61deb4bb9ffdb98e6268
Scalfi <gascalfi@gmail.com>
2020-11-02 22:15:54 -0300

    create base view produto controller

--- Controllers/PagesController/ProdutoPageController.cs
+++ Controllers/PagesController/ProdutoPageController.cs
@@ -7,81 +7,13 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Learn.Controllers.PagesController
 {
+    [Route("Produto")]
     public class ProdutoPageController : Controller
     {
         // GET: ProdutoPageController
-        public ActionResult Index()
+        public IActionResult Index()
         {
             return View();
-        }
-
-        // GET: ProdutoPageController/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        // GET: ProdutoPageController/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: ProdutoPageController/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: ProdutoPageController/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: ProdutoPageController/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: ProdutoPageController/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: ProdutoPageController/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
         }
     }
 }
